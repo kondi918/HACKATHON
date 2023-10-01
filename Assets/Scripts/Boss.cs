0bb7eb21356f0596b4833d9210f9d9f14fbd1a4d
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2023-10-01 07:02:53 +0200

    Boss
    The triple shot

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -10,10 +10,20 @@ public class Boss : MonoBehaviour
     [SerializeField] GameObject room;
     float summonCD = 0;
 
+    [SerializeField] GameObject projectileExample;
+    //[SerializeField] float reload = 1f;
+    [SerializeField] float projectileSpeed = 10f;
+    private Vector3 direction = Vector3.zero;
+
+    [SerializeField] float movingSpeed = 1f;
+
+    [SerializeField] GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         gameController.SetCurrentRoom(1);
+        StartCoroutine(shoting());
     }
 
     // Update is called once per frame
@@ -43,5 +53,34 @@ public class Boss : MonoBehaviour
         newSidekick.transform.SetParent(room.transform);
         newSidekick.transform.position = new Vector3(Random.Range(-8, 8), Random.Range(-8, 8));
         newSidekick.SetActive(true);
+    }
+
+    private void Shot(float degree)
+    {
+        Quaternion rotationQuaternion = Quaternion.Euler(0, 0, degree);
+
+        direction = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0) - GetPlayerPosition();
+
+        GameObject projectile = Instantiate(projectileExample);
+        projectile.transform.SetLocalPositionAndRotation(gameObject.transform.position, gameObject.transform.rotation);
+        projectile.SetActive(true);
+        projectile.GetComponent<Rigidbody2D>().velocity = rotationQuaternion * direction.normalized * projectileSpeed * -1;
+        Destroy(projectile, 5f);
+    }
+
+    
+
+    private Vector3 GetPlayerPosition()
+    {
+        return new Vector3(player.transform.position.x, player.transform.position.y, 0);
+    }
+
+    IEnumerator shoting()
+    {
+        yield return new WaitForSeconds(UnityEngine.Random.Range(1f, 3f));
+        Shot(0);
+        Shot(15);
+        Shot(-15);
+        StartCoroutine(shoting());
     }
 }
